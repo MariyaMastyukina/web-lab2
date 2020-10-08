6214bbb358ee124dedfd3e4601497fe2676fa624
Kir Tankovskiy <tankovskiyko@gmail.com>
2020-10-08 20:01:51 +0300

    Refactoring js code

--- src/main/webapp/web/js/SVGclick.js
+++ src/main/webapp/web/js/SVGclick.js
@@ -1,44 +1,24 @@
-document.getElementById("svg").addEventListener('click',function (e){
-    var svg =document.getElementById("svg");
+document.getElementById("svg").addEventListener('click', function (e) {
+    console.log("click");
+    var svg = document.getElementById("svg");
     var HTMLPoint = svg.createSVGPoint();
     HTMLPoint.x = e.clientX;
     HTMLPoint.y = e.clientY;
     var SVGPoint = HTMLPoint.matrixTransform(svg.getScreenCTM().inverse());
-    var cx=SVGPoint.x;
-    var cy=SVGPoint.y;
-    var r_value=$('.r_value').val();
+    var cx = SVGPoint.x;
+    var cy = SVGPoint.y;
+    var r_value = $('.r_value').val();
     var regExp = new RegExp("^(-?(?:(?:\\d+|\\d*\\.\\d+)(?:[e][+|-]?\\d+)?))+");
-    if (regExp.test(r_value)&&r_value>2&&r_value<5){
-        document.getElementById("point").setAttribute("cx",String(cx));
-        document.getElementById("point").setAttribute("cy",String(cy));
-        document.getElementById("point").setAttribute("r","3");
-        var x_value=(cx-150)*r_value/100;
-        var y_value=((150-cy)*r_value/100).toFixed();
-        let form_data = "y_value=" + encodeURIComponent(y_value)+"&x_value=" + encodeURIComponent(x_value)+"&r_value=" + encodeURIComponent(r_value);
-        $.ajax(({
-            type: 'POST',
-            url: 'control',
-            data: form_data + "&reset=" + encodeURIComponent(localStorage.getItem("reset_flag")),
-            success: function (e) {
-                console.log("success");
-                console.log(e.toString());
-                localStorage.setItem("reset_flag", "false");
-                if (e.toString().includes("<html>") || e.toString().includes("error")) {
-                    // say to user that he didnt choose x or r
-                } else {
-                    document.querySelector("#result").innerHTML = e.toString();
-                    saveUpdateTable(e.toString());
-                }
-
-            },
-            error: function (e) {
-                console.log("error");
-                alert("error");
-                //TODO error handler
-            }
-        }));
-    }
-    else{
+    console.log(checkY(((150 - cy) * r_value / 100).toFixed()) + " y check");
+    if (checkX(((cx - 150) * r_value / 100)) && checkY(((150 - cy) * r_value / 100).toFixed()) && regExp.test(r_value) && checkR(r_value)) {
+        document.getElementById("point").setAttribute("cx", String(cx));
+        document.getElementById("point").setAttribute("cy", String(cy));
+        document.getElementById("point").setAttribute("r", "3");
+        let x_value = (cx - 150) * r_value / 100;
+        let y_value = ((150 - cy) * r_value / 100).toFixed();
+        let form_data = "y_value=" + encodeURIComponent(y_value) + "&x_value=" + encodeURIComponent(x_value) + "&r_value=" + encodeURIComponent(r_value);
+        send(form_data);
+    } else {
         alert("Действие невозможно! Введите валидное значение для R");
     }
 });
--- src/main/webapp/web/js/main.js
+++ src/main/webapp/web/js/main.js
@@ -1,20 +1,19 @@
 let x_value = $(".x_value");
 let r_value = $(".r_value");
-let y_value=0;
+let y_value = 0;
 let submit_button = $("#submit_button");
 let y_group = $('input[name = y_value]');
 let reset_button = $("#reset_button");
 let regExp = new RegExp("^(-?(?:(?:\\d+|\\d*\\.\\d+)(?:[e][+|-]?\\d+)?))+");
 let default_table = document.querySelector("#result").innerHTML;
 submit_button.click((e) => {
-    //Checking odz is done
     e.preventDefault();
-    if (checkODZ()) {
+    if (checkODZ(x_value.val(), r_value.val())) {
         let form_data;
         for (let i = 0; i < y_group.length; i++) {
             if (y_group[i].checked) {
                 form_data = "y_value=" + encodeURIComponent(y_group[i].value);
-                y_value=y_group[i].value;
+                y_value = y_group[i].value;
             }
         }
         if (x_value.val() !== "") {
@@ -23,51 +22,35 @@ submit_button.click((e) => {
         if (r_value.val() !== "") {
             form_data += "&r_value=" + encodeURIComponent(r_value.val());
         }
-        var cx=x_value.val()/r_value.val()*100+150;
-        var cy=150-y_value/r_value.val()*100;
-        alert(cx+" "+cy);
-        document.getElementById("point").setAttribute("cx",String(cx));
-        document.getElementById("point").setAttribute("cy",String(cy));
-        document.getElementById("point").setAttribute("r","3");
+        var cx = x_value.val() / r_value.val() * 100 + 150;
+        var cy = 150 - y_value / r_value.val() * 100;
+        send(form_data);
+        alert(cx + " " + cy);
+        document.getElementById("point").setAttribute("cx", String(cx));
+        document.getElementById("point").setAttribute("cy", String(cy));
+        document.getElementById("point").setAttribute("r", "3");
         console.log(form_data);
-        $.ajax(({
-            type: 'POST',
-            url: 'control',
-            data: form_data + "&reset=" + encodeURIComponent(localStorage.getItem("reset_flag")),
-            success: function (e) {
-                console.log("success");
-                console.log(e.toString());
-                localStorage.setItem("reset_flag", "false");
-                if (e.toString().includes("<html>") || e.toString().includes("error")) {
-                    // say to user that he didnt choose x or r
-                } else {
-                    document.querySelector("#result").innerHTML = e.toString();
-                    saveUpdateTable(e.toString());
-                }
 
-            },
-            error: function (e) {
-                console.log("error");
-                alert("error");
-                //TODO error handler
-            }
-        }));
     } // else { ??? }
 });
 
-function checkX() {
-    return regExp.test(x_value.val()) && x_value.val() > -3 && x_value.val() < 5;
+function checkX(x_value) {
+    console.log(x_value + " x_value");
+    return regExp.test(x_value) && x_value > -3 && x_value < 5;
 }
 
-function checkR() {
-    return regExp.test(r_value.val()) && r_value.val() > 2 && r_value.val() < 5;
+function checkR(r_value) {
+    return regExp.test(r_value) && r_value > 2 && r_value < 5;
+}
+function checkY(y_value) {
+    return regExp.test(y_value) && y_value >= -5 && y_value <= 3;
 }
 
-function checkODZ() {
+function checkODZ(x_value, r_value) {
     //the alert needs to be changed to something
-    if (!checkX()) alert("Введены некорректные значения для параметра Х");
-    if (!checkR()) alert("Введены некорректные значения для параметра R");
-    return checkR() && checkX();
+    if (!checkX(x_value)) alert("Введены некорректные значения для параметра Х");
+    if (!checkR(r_value)) alert("Введены некорректные значения для параметра R");
+    return checkR(r_value) && checkX(x_value);
 }
 
 function saveUpdateTable(table) {
@@ -76,11 +59,36 @@ function saveUpdateTable(table) {
 
 reset_button.click(e => {
     e.preventDefault();
-    document.getElementById("point").setAttribute("r","0");
+    document.getElementById("point").setAttribute("r", "0");
     document.getElementById("result").innerHTML = default_table;
     localStorage.setItem("current_table", default_table);
     localStorage.setItem("reset_flag", "true");
 });
 if (window.performance) {
     document.getElementById("result").innerHTML = localStorage.getItem("current_table");
+}
+
+function send(form_data) {
+    $.ajax(({
+        type: 'POST',
+        url: 'control',
+        data: form_data + "&reset=" + encodeURIComponent(localStorage.getItem("reset_flag")),
+        success: function (e) {
+            console.log("success");
+            console.log(e.toString());
+            localStorage.setItem("reset_flag", "false");
+            if (e.toString().includes("<html>") || e.toString().includes("error")) {
+                // say to user that he didnt choose x or r
+            } else {
+                document.querySelector("#result").innerHTML = e.toString();
+                saveUpdateTable(e.toString());
+            }
+
+        },
+        error: function (e) {
+            console.log("error");
+            alert("error");
+            //TODO error handler
+        }
+    }));
 }
